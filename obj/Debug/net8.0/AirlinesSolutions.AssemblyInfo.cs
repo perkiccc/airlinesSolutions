fa6af5bc49dfeff51ea3626d64a35442c3e1eac1
nace <nace.koprivec@gmail.com>
2024-05-22 23:30:21 +0200

    koncna verzija

--- obj/Debug/net8.0/AirlinesSolutions.AssemblyInfo.cs
+++ obj/Debug/net8.0/AirlinesSolutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirlinesSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b08d5188e29d20e3fc95e1de743a166f3ebfab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c1194a60898c1e3d6d2357ef9d2b016757e9d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirlinesSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirlinesSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
